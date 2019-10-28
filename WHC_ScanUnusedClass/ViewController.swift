040e974e78db0362e66f1653515689fb74b2239b
张海明 <1938708066@qq.com>
2019-10-28 13:35:43 +0800

    swift

--- WHC_ScanUnusedClass/ViewController.swift
+++ WHC_ScanUnusedClass/ViewController.swift
@@ -113,18 +113,18 @@ class ViewController: NSViewController {
     }
     
     @IBAction func clickCheckUpdate(_ sender: NSButton) {
-        NSWorkspace.shared().open(URL(string: "https://github.com/netyouli/WHC_ScanUnreferenceImageTool")!)
+        NSWorkspace.shared.open(URL(string: "https://github.com/netyouli/WHC_ScanUnreferenceImageTool")!)
     }
     
     @IBAction func clickAbout(_ sender: NSButton) {
-        NSWorkspace.shared().open(URL(string: "https://github.com/netyouli/")!)
+        NSWorkspace.shared.open(URL(string: "https://github.com/netyouli/")!)
     }
     
     @IBAction func clickOpenDirectory(_ sender: NSButton) {
         let openPanel = NSOpenPanel()
         openPanel.canChooseFiles = false
         openPanel.canChooseDirectories = true
-        if openPanel.runModal() == NSModalResponseOK {
+        if openPanel.runModal() == NSApplication.ModalResponse.OK {
             directoryText.stringValue = (openPanel.directoryURL?.path)!
         }
     }
@@ -137,10 +137,10 @@ class ViewController: NSViewController {
         stopScan = !stopScan
         sender.title = stopScan ? "停止扫描" : "开始扫描"
         if !stopScan {return}
-        if fastRadio.state == 1 {scanLevel = .fast}
-        else if normalRadio.state == 1 {scanLevel = .normal}
+        if fastRadio.state.rawValue == 1 {scanLevel = .fast}
+        else if normalRadio.state.rawValue == 1 {scanLevel = .normal}
         else {scanLevel = .carefully}
-        if (iOSRadio.state == 1) {
+        if (iOSRadio.state.rawValue == 1) {
             scanProjectType = .iOS
         }else {
             scanProjectType = .android
@@ -197,16 +197,16 @@ class ViewController: NSViewController {
                     alert.addButton(withTitle: "保存")
                     alert.addButton(withTitle: "取消")
                     alert.beginSheetModal(for: self.view.window!, completionHandler: { (modalResponse) in
-                        if modalResponse == 1000 {
+                        if modalResponse.rawValue == 1000 {
                             let savaPanel = NSSavePanel()
                             savaPanel.message = "Choose the path to save the document"
                             savaPanel.allowedFileTypes = ["txt"]
                             savaPanel.allowsOtherFileTypes = false
                             savaPanel.canCreateDirectories = true
                             savaPanel.beginSheetModal(for: self.view.window!, completionHandler: {[unowned self] (code) in
-                                if code == 1 {
+                                if code.rawValue == 1 {
                                     do {
-                                        let originTxt = self.notUseClassResultContentView.string == nil ? "" : self.notUseClassResultContentView.string!
+                                        let originTxt = self.notUseClassResultContentView.string == nil ? "" : self.notUseClassResultContentView.string
                                         try originTxt.write(toFile: savaPanel.url!.path, atomically: true, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))
                                     }catch {
                                         print("写文件异常")
@@ -556,7 +556,7 @@ class ViewController: NSViewController {
     fileprivate func execScan(className: String) {
         autoreleasepool {
             DispatchQueue.main.sync {
-                let originTxt = self.resultContentView.string == nil ? "" : self.resultContentView.string!
+                let originTxt = self.resultContentView.string == nil ? "" : self.resultContentView.string
                 let allClassName = ">>>>> " + className + "\n"
                 self.setResultContent(content: originTxt + allClassName)
                 self.progressLabel.stringValue = className
@@ -620,7 +620,7 @@ class ViewController: NSViewController {
             if !isReference {
                 DispatchQueue.main.sync(execute: {
                     self.notUseClassCount += 1
-                    let originTxt = self.notUseClassResultContentView.string == nil ? "" : self.notUseClassResultContentView.string!
+                    let originTxt = self.notUseClassResultContentView.string == nil ? "" : self.notUseClassResultContentView.string
                     self.setNotUseResultContent(content: originTxt + ">>>>> " + className + "\n")
                     self.notUseClassCountLabel.stringValue = "项目没使用的类: 总计\(self.notUseClassCount)个"
                 })
